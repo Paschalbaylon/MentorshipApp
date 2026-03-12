b763160f5d72ff575a28e720fbafffc1e49e23f4
Paschalbaylon <97518763+Paschalbaylon@users.noreply.github.com>
2026-03-12 16:32:33 +0100

    Fix the net10

--- DSA Project/WebApp/obj/Debug/net10.0/WebApp.AssemblyInfo.cs
+++ DSA Project/WebApp/obj/Debug/net10.0/WebApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5bce0cf5d13b37434c32e9fb05fda070b41b447")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8a2afe8fc11a4b986612ddde3e441d2932c3908")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
