7c0dd5e28be01d9edbd53496c46870672baaafce
Paschalbaylon <97518763+Paschalbaylon@users.noreply.github.com>
2026-03-13 16:48:08 +0100

    any changes

--- DSA Project/WebApp/obj/Debug/net10.0/WebApp.AssemblyInfo.cs
+++ DSA Project/WebApp/obj/Debug/net10.0/WebApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aac969aecbe17e1c725049a2635f07c077fd45d0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+525d45d33833ba68f82702de6b48c7fe9995eb4b")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
