5734d349292ae22812ec42398abe0cc24cc66714
Paschalbaylon <97518763+Paschalbaylon@users.noreply.github.com>
2026-03-19 22:26:17 +0100

    route fix

--- DSA FronEnd/my-dsa/src/api/auth.js
+++ DSA FronEnd/my-dsa/src/api/auth.js
@@ -35,7 +35,8 @@ export async function loginUser(credentials) {
 
 export async function admin() {
   try {
-    const response = await axiosInstance.get("/../admin/User");
+    // const response = await axiosInstance.get("/../admin/User");
+    const response = await axiosInstance.get("/api/admin/User");
     return response.data;
   } catch (error) {
     console.error(
