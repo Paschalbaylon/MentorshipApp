7960e75e469aac9ef09b64e79bdd9aa5b27482cc
Paschalbaylon <97518763+Paschalbaylon@users.noreply.github.com>
2026-03-19 11:47:55 +0100

    add import

--- DSA FronEnd/my-dsa/src/Pages/ProfilePage.jsx
+++ DSA FronEnd/my-dsa/src/Pages/ProfilePage.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axiosInstance from "../api/axiosInstance";
+import { getMyProfile } from "../api/auth";
 
 // const ProfilePage = () => {
 //   const [profile, setProfile] = useState({
